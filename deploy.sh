f5ef2980e485920da6373eddd2f711d9b43134c4
Srijan R Shetty <srijan.shetty@gmail.com>
2020-11-30 12:27:05 +0530

    build: update build process

--- deploy.sh
+++ deploy.sh
@@ -2,27 +2,8 @@
 
 set -e
 
-branch=$(git rev-parse --abbrev-ref HEAD)
+xelatex cv
+cp cv.pdf docs/
 
-# if [[ $branch == "master" ]]; then
-#     xelatex cv
-#     git checkout gh-pages
-#     # pdf2htmlEX --process-outline 0 --zoom 1.6 cv.pdf
-#     # mv cv.html index.html
-#     # git add index.html
-#     git add cv.pdf
-#     git commit -m "build(auto): Update to Website"
-#     git push origin gh-pages
-#     git checkout master
-# fi
-
-if [[ $branch == "master" ]]; then
-    xelatex cv
-    cp cv.pdf index.pdf
-    git checkout gh-pages
-    mv index.pdf cv.pdf
-    git add cv.pdf
-    git commit -m "build(auto): Update to Website"
-    git push origin gh-pages
-    git checkout master
-fi
+xelatex resume
+cp resume.pdf docs/
